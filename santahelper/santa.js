644411c6fa1d49be27dccbf7a998acf3176262d9
LoganRoth <logantheroth@gmail.com>
2021-09-09 20:33:23 -0400

    Speeding it up

--- santahelper/santa.js
+++ santahelper/santa.js
@@ -1,35 +1,39 @@
 const fs = require('fs')
 
-const file = fs.readFileSync('./data.txt', err => {
-    if (err) {
-        console.log(err)
-    }
-}).toString();
-console.time('Advent of Code 2015, Day 1');
+function santaChallenege() {
+    const file = fs.readFileSync('./data.txt', err => {
+        if (err) {
+            console.log(err)
+        }
+    }); // Could do .toString() if wanted, but this is faster
+    console.time('Advent of Code 2015, Day 1');
 
-let floor = 0;
-let basement = 0;
-let set = false;
+    let floor = 0;
+    let basement = 0;
+    let set = false;
 
-// Iterate over the returned string
-[...file].forEach((character, i) => {
-    if (character === '(') {
-        floor++;
-    } else { // thing is ')'
-        floor--;
-    }
-    if (!set && (floor === -1)) { // only set once
-        set = true;
-        basement = i + 1; // since answer is 1 indexed
-    }
-})
+    // Iterate over the file
+    // Could do [...file] if file was a string
+    file.forEach((character, i) => {
+        if (character === 40) { // code for '('
+            floor++;
+        } else { // thing is ')'
+            floor--;
+        }
+        if (!set && (floor === -1)) { // only set once
+            set = true;
+            basement = i + 1; // since answer is 1 indexed
+        }
+    })
 
-console.timeEnd('Advent of Code 2015, Day 1')
-console.log('The instructions lead him to floor: ' + floor)
-console.log('The instruction that first makes him hit the -1st floor is: ' + basement)
+    console.timeEnd('Advent of Code 2015, Day 1')
+    console.log('The instructions lead him to floor: ' + floor)
+    console.log('The instruction that first makes him hit the -1st floor is: ' + basement)
+}
 
+santaChallenege();
 /*
-Advent of Code 2015, Day 1: 0.569ms
+Advent of Code 2015, Day 1: 0.438ms
 The instructions lead him to floor: 232
 The instruction that first makes him hit the -1st floor is: 1783
 */
